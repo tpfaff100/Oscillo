d8687aec3b45c51f6d64a8f1e2b3c0b8621db89c
tcp <thomas@notastfastie.local>
2025-03-30 02:45:35 -0700

    added support for settings that are stored in "preset" files.

--- 20240310Ver01/Preset.cpp
+++ 20240310Ver01/Preset.cpp
@@ -7,10 +7,14 @@
 #include <unistd.h>	// std::usleep
 
 #include "Preset.h"
+#include "Bank.h"
 
-#define TAG_DELAY	"delay"
-#define TAG_DURATION	"duration"
-#define TAG_FILE	"file"
+#define TAG_DELAY		"initial_delay"
+#define TAG_DURATION		"duration"
+#define TAG_FILE		"file"
+#define TAG_LOOP_YIELD_DELAY	"loop_yield_time"
+#define TAG_OSC1_INC_RATE	"osc1_inc_rate"
+#define TAG_OSC2_INC_RATE	"osc2_inc_rate"
 
 
 using namespace std;
@@ -89,41 +93,61 @@ Preset::Preset(std::string filename) {
 }
 
 // we get a tag/string with units in it.  We look at the units
-// and convert to milleseconds because that is what usleep() takes.
+// and convert to microseconds because that is what usleep() takes.
 // supported units:  (s)econds and (ms) milleseconds
 int Preset::unitsToms(string data, string tag) {
         string num = first_number_in_str(appsets[tag]);
         string units = first_units_in_str(appsets[tag]);
         int time_in_ms = std::stoi(num);	// currently time in whatever units specified.
-	
-	if (units == "s") 		// now convert to milleseconds and return.
+
+	if (units == "ms")
 		time_in_ms *= 1000;
+	if (units == "s") 		// now convert to milleseconds and return.
+		time_in_ms *= 1000000;
 	return time_in_ms;
 }
 
 int Preset::run() {
-#if 0
-	cout << appsets["delay"] << "\n";
-	cout << appsets["file"] << "\n";
-	cout << appsets["duration"] << "\n";
-#endif
-
-	uint64_t delay = unitsToms( appsets["delay"], TAG_DELAY);
+	uint64_t delay = 		unitsToms( appsets[TAG_DELAY], TAG_DELAY );
 	usleep(delay);
-	uint64_t duration = unitsToms( appsets["duration"], TAG_DURATION);
 
-	std::cout << timeSinceEpochMillisec() << std::endl;
+	uint64_t duration = 		unitsToms( appsets[TAG_DURATION], TAG_DURATION );
+	uint64_t presetLoopYield = 	unitsToms( appsets[TAG_LOOP_YIELD_DELAY], TAG_LOOP_YIELD_DELAY );
 
-	long long expiry_time = timeSinceEpochMillisec() + duration,cur_time = -1;;
+//	std::cout << timeSinceEpochMillisec() << std::endl;
+
+	long long expiry_time = timeSinceEpochMillisec() + duration/1000, cur_time = 0;
+
+	float osc1_inc_rate = std::stof(appsets[TAG_OSC1_INC_RATE]);
+
+cout << "expiry time:  " << expiry_time << "\n";
+
+        Oscill osc_ary[] = { Oscill(osc1_inc_rate) };
+        int osc_count = sizeof(osc_ary) / sizeof(osc_ary[0]);
+        Bank *bank1 = new Bank(osc_ary, osc_count);
+        Sprite *sprite = new Sprite(appsets[TAG_FILE], 23);
 
 	do {
 		cur_time = timeSinceEpochMillisec();
-		usleep(1000000);
-		cout << cur_time << "   " << expiry_time << "\n";;
+		cout << cur_time << "   " << expiry_time << "\n";
+
+		bank1->setSprite(sprite);       // attach the animation to the oscillator bank.
+
+		while(bank1->range()) {
+			bank1->dump();          // iterate and draw waveforms and animation.
+			usleep(presetLoopYield);
+			std::system("clear");
+			bank1->clear();
+        }
+
+	delete sprite;
+        delete bank1;
+
 	}
 	while(expiry_time - cur_time > 0);
 
-
 	return 0;
 };
+
+
 
